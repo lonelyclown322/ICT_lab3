2633b2310320de32c26514cc9fd6e1146b80cbfa
Sergey <Lazer2004serg@gmail.com>
2021-10-13 23:12:06 +0300

    Update AdditionalTask1.py

--- task2/AdditionalTask1.py
+++ task2/AdditionalTask1.py
@@ -1,35 +1,26 @@
+import re
 #335106 % 6 = 0
 
 #Создаем функцию для проверки файла на хайку
 def checkIfHaiku (filename):
-    vowels = ['а','о','э','е','и','ы','у','ё','ю','я']
     fileToCheck = open(filename,'r', encoding='utf-8-sig')
-    wholeTextInOnestring = ''
-    #разберем текст и заключим его в одну строку
-    for string in fileToCheck:
-        wholeTextInOnestring += string.strip().lower()
-    #создадим массив строк на основе деления через /
-    arrayOfHaikuStrings = wholeTextInOnestring.split('/')
+    arrayOfHaikuStrings = fileToCheck.read().split('/')
+    pattern = re.compile('а|о|э|е|и|ы|у|ё|ю|я|А|О|Э|Е|И|Ы|У|Ё|Ю|Я')
     #Проверяем является ли хайку, для этого сначала проверяем количество строк в массиве ?= 3
     if len(arrayOfHaikuStrings) != 3:
         return('Не хайку. Должно быть 3 строки.')
-    else: #иначе проходимся по каждой строке и проверяем количество согласных, если строка подоходит,
-        #  то прибавляем к счетчику 1. Если текст является хайку, то счетчик в конце цикла должен быть равен 3
-        #  иначе текст не является хайку
+    else: #иначе химичим :)
         check3strings = 0
-        for i in range (len(arrayOfHaikuStrings)):
-            countVowels = 0
-            for elem in arrayOfHaikuStrings[i]:
-                if elem in vowels:
-                    countVowels += 1
-            if (i == 0 or i == 2) and countVowels == 5:
+        for i in range(3):
+            if (len(re.findall(pattern, arrayOfHaikuStrings[i])) == 5) and (i == 0 or i == 2):
                 check3strings += 1
-            if i == 1 and countVowels == 7:
+            elif (len(re.findall(pattern, arrayOfHaikuStrings[i])) == 7) and (i == 1):
                 check3strings += 1
         if check3strings == 3:
-            return('Хайку!')
+            return ('Хайку!')
         else:
-            return('Не хайку.')
+            return ('Не хайку.')
+
 
 #Запускам пять тестов для проверки написанной функциий
 for i in range(1,6):
