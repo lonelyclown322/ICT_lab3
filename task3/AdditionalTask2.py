144f076aca1225fbe4c6d4a61cc258904741e3b2
Sergey <Lazer2004serg@gmail.com>
2021-10-19 00:19:22 +0300

    Update AdditionalTask2.py

--- task3/AdditionalTask2.py
+++ task3/AdditionalTask2.py
@@ -2,30 +2,30 @@
 import re
 
 #описываем функцию для замены найденного числа
-def replaceFunction(stringNumber):
-    x = int(stringNumber)
+def replace_function(string_number):
+    x = int(string_number)
     return str(3 * x**2 + 5)
 
 #создаем функцию шифровки
-def encryptMessage(filename):
-    fileToCheck = open(filename,'r', encoding='utf-8-sig')
-    fileToStrings = []
+def encrypt_message(filename):
+    file_to_check = open(filename,'r', encoding='utf-8-sig')
+    file_to_strings = []
     pattern = re.compile('[0-9]+')#создаем паттерн поиска числа
     #заполняем массив fileToStrings строками из файла
-    for string in fileToCheck:
-        fileToStrings.append(string.rstrip())
+    for string in file_to_check:
+        file_to_strings.append(string.rstrip())
     #проходимся по массиву строк из файла и с помощью регулярного выражения находим все числа и заменяем их при помощи функции
-    for i in range(len(fileToStrings)):
-        numbersInString = re.findall(pattern, fileToStrings[i])
-        for number in numbersInString:
-            fileToStrings[i] = fileToStrings[i].replace(number, replaceFunction(number))
+    for i in range(len(file_to_strings)):
+        numbers_in_string = re.findall(pattern, file_to_strings[i])
+        for number in numbers_in_string:
+            file_to_strings[i] = file_to_strings[i].replace(number, replace_function(number))
     #выводим строки на экран
-    for string in fileToStrings:
+    for string in file_to_strings:
         print(string)
         
 #Запускам пять тестов для проверки написанной функциий
 for i in range(1,6):
     print('-----New Test-----')
-    currentTest = 'test' + str(i) + '.txt'
-    encryptMessage(currentTest)
+    current_test = 'test' + str(i) + '.txt'
+    encrypt_message(current_test)
     
